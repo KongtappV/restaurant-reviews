8830cc0dc05ffadc36b70aa5f0989bd2496bbf05
KongtappV <kongtapp.v@ku.th>
2022-01-27 08:28:45 +0700

    fix restaurant apiroute

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -5,6 +5,9 @@ import ReviewCtrl from "./reviews.controller.js"
 const router = express.Router()
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
+router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
+
 router.
     route("/reviews")
     .post(ReviewCtrl.apiPostReview)
