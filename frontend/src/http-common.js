a9cf762ba120788818981bf3bb38c17958acee6e
KongtappV <kongtapp.v@ku.th>
2022-01-30 23:44:11 +0700

    fixed

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://us-east-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-reviews-evuay/service/restaurants/incoming_webhook/",
-//   baseURL: "https://ap-southeast-1.aws.data.mongodb-api.com/app/restaurant-review-yzabh/endpoint/",
+    baseURL: "https://ap-southeast-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/restaurant-review-yzabh/service/Restaurants-Reviews/incoming_webhook/",
     headers: {
         "Content-type": "application/json"
     }
