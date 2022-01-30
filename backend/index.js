f220963432b4f33c9c5381d83ce89a899b42daf7
KongtappV <kongtapp.v@ku.th>
2022-01-30 18:01:03 +0700

    add frontend

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ MongoClient.connect(
     process.env.RESTREVIEWS_DB_URI,
     {
         // poolSize: 50,
-        wtimeout: 2500,
+        wtimeoutMS: 2500,
         // useNewUrlParse: true
     }
 )
